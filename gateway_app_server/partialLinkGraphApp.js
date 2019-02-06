1c258f19b78463c429584de45339d85471798f2c
Nabeel Nasir <nabeeln7@gmail.com>
2019-02-06 15:48:48 -0500

    partialLinkGraphApp returning devices active only in the last 5mins

--- gateway_app_server/partialLinkGraphApp.js
+++ gateway_app_server/partialLinkGraphApp.js
@@ -6,13 +6,12 @@ const MongoClient = require('mongodb').MongoClient;
 const mongo_url = 'mongodb://localhost:27017';
 const dbName = 'discovery';
 
-//TODO: filter for devices present in the last 15mins
 async function getPartialLinkGraph() {
-	// console.log(`looking for time > ${Date.now() - 900000}`)
+	//returning devices active in the last 5minutes
 	const conn = await MongoClient.connect(mongo_url, { useNewUrlParser: true });
 	const db = await conn.db(dbName);
 	const plg = await db.collection('partialLinkGraph')
-						.find({})
+						.find({"ts": {$gt: Date.now() - 300000}})
 						.project({"ts":0})
 						.toArray();
 	return plg;
