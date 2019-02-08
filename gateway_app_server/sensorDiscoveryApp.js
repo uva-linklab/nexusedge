9f39f771b328ec14b55e2c8d44cd3c34a3a4a01d
Nabeel Nasir <nabeeln7@gmail.com>
2019-02-08 18:57:12 -0500

    minor changes

--- gateway_app_server/sensorDiscoveryApp.js
+++ gateway_app_server/sensorDiscoveryApp.js
@@ -10,7 +10,7 @@ const sensor_discovery_collection = 'sensor_discovery';
 async function getAttachedSensors() {
 	//returns sensors active in the last 5minutes
 	const client = await MongoClient.connect(mongo_url, { useNewUrlParser: true });
-	const db = await conn.db(dbName);
+	const db = await client.db(dbName);
 	const sensors = await db.collection(sensor_discovery_collection)
 						.find({"ts": {$gt: Date.now() - 300000}})
 						.project({"ts":0})
