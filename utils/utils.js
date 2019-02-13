ea1ecb75222b0f4b901fde89c177f4061790f0a7
Nabeel Nasir <nabeeln7@gmail.com>
2019-02-13 13:13:34 -0500

    adding a config file for utils

--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,7 @@ module.exports.logWithTs = logWithTs;
 module.exports.getIPAddress = getIPAddress;
 //const ip = require('ip');
 const pcap = require('pcap');
+const config = require('./config/client.json');
 
 function logWithTs(log) {
 	console.log(`[${getCurrentDateTime()}] ${log}`);
@@ -15,7 +16,14 @@ function getCurrentDateTime() {
 //	return ip.address();
 //}
 
-//TODO move the wlan0 harcoding to a config file
 function getIPAddress() {
-	return pcap.findalldevs().filter(entry => entry.name === "wlan0")[0].addresses.filter(entry => entry.addr != "")[0].addr;
+	const interface = config.network.interface;
+	if(!interface) {
+		logWithTs("interface not found in config file");
+	}
+	return pcap.findalldevs()
+				.filter(entry => entry.name === interface)[0]
+				.addresses
+				.filter(entry => entry.addr != "")[0]
+				.addr;
 }
