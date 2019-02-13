2ca2c26a5413b52cc86e08418dad8e5ff07114b5
Nabeel Nasir <nabeeln7@gmail.com>
2019-02-13 00:33:28 +0000

    hack: using pcap instead of ip for getting the ip address. on the artik boards, os.networkInterfaces() does not list wlan0 interface and hence the ip module doesn't work. known issue in nodejs os module.

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,7 @@
 module.exports.logWithTs = logWithTs;
 module.exports.getIPAddress = getIPAddress;
-const ip = require('ip');
+//const ip = require('ip');
+const pcap = require('pcap');
 
 function logWithTs(log) {
 	console.log(`[${getCurrentDateTime()}] ${log}`);
@@ -10,6 +11,11 @@ function getCurrentDateTime() {
 	return new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
 }
 
+//function getIPAddress() {
+//	return ip.address();
+//}
+
+//TODO move the wlan0 harcoding to a config file
 function getIPAddress() {
-	return ip.address();
+	return pcap.findalldevs().filter(entry => entry.name === "wlan0")[0].addresses.filter(entry => entry.addr != "")[0].addr;
 }
