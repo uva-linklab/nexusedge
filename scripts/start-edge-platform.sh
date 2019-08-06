a3721a3e73e4588b8044a375ed8c5f96d1fabb6c
Nabeel Nasir <nabeeln7@gmail.com>
2019-08-06 18:32:40 -0400

    added enocean script to the startup script

--- scripts/start-edge-platform.sh
+++ scripts/start-edge-platform.sh
@@ -1,8 +1,14 @@
-node on-the-edge/gateway_app_server/server.js > on-the-edge/logs/gw_app_server.log &
-node on-the-edge/gateway_code/gateway.js f > on-the-edge/logs/gw_code.log &
-node on-the-edge/sensor_discover/discover.js &
-node on-the-edge/ble-peripheral-discovery/scan.js &
+#on-the-edge scripts
+node $HOME/on-the-edge/gateway_app_server/server.js > $HOME/on-the-edge/logs/gw_app_server.log &
+node $HOME/on-the-edge/gateway_code/gateway.js f > $HOME/on-the-edge/logs/gw_code.log &
+node $HOME/on-the-edge/sensor_discover/discover.js &
+node $HOME/on-the-edge/ble-peripheral-discovery/scan.js &
+
 #lab11 gateway script
-node gateway/software/ble-gateway-mqtt/ble-gateway-mqtt.js &
+node $HOME/gateway/software/ble-gateway-mqtt/ble-gateway-mqtt.js &
+
 #service-api
-node service-framework/http-api-server/server.js > service-framework/logs/server.log &
+node $HOME/service-framework/http-api-server/server.js > $HOME/service-framework/logs/server.log &
+
+#receive enocean packets
+node $HOME/gateway/software/enocean-generic-gateway/enocean-generic-gateway.js &
