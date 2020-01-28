acababdf66e7277bb654cd0aedbe152026a59e83
Nabeel Nasir <nabeeln7@gmail.com>
2020-01-27 19:52:39 -0500

    remove unwanted files

--- gateway_code/cron_test.js
+++
@@ -1,6 +0,0 @@
-var cron = require('cron');
-var cronJob = cron.job("*/5 * * * * *", function(){
-    // perform operation e.g. GET request http.get() etc.
-    console.log('cron job completed');
-}); 
-cronJob.start();
