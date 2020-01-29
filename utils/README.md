1332a5694a37e471dddd4d71225375cffe63bc3a
Nabeel Nasir <nabeeln7@gmail.com>
2020-01-28 22:08:13 -0500

    remove config file from git

--- utils/README.md
+++ utils/README.md
@@ -1,4 +1,14 @@
-Common utils for projects
+Common utilities for projects
+
+Setup:
+1. Add a config file named config.json which contains the network interface name that the gateway uses.
+
+For e.g.:
+{
+  "network": {
+    "interface": "en0"
+  }
+}
 
 Installation:
 apt-get install libpcap-dev
--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,12 @@
 module.exports.getIPAddress = getIPAddress;
 
 const pcap = require('pcap');
-const config = require('./config/client.json');
+const config = require('./config.json');
 
 function getIPAddress() {
 	const interface = config.network.interface;
 	if(!interface) {
-		logWithTs("interface not found in config file");
+		console.log("interface not found in config file");
 	}
 
 	//regex to exclude ipv6 addresses and only capture ipv4 addresses. This doesn't ensure that the ipv4 octets are 0-255 but this would suffice. All we need is to exclude ipv6 addresses. 
