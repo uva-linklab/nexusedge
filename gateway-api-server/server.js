0749d6fb50f07de253d7d20dcb1d4c4004b730d9
Nabeel Nasir <nabeeln7@gmail.com>
2020-02-07 12:32:15 -0500

    uncommenting the code deployer stuff

--- gateway-api-server/server.js
+++ gateway-api-server/server.js
@@ -30,37 +30,37 @@ app.listen(port, function() {
   console.log(`Gateway API server started on port ${port}`)
 });
 
-//throw an error if it is an unknown endpoint
-app.use(function(req, res) {
-  res.status(404).send(`${req.originalUrl} is not a valid endpoint.`);
+//TODO code deployer stuff move these to routes and give it a proper controller
+
+const deployedCodePath = `${__dirname}/deployed-code/`;
+if (!fs.existsSync(deployedCodePath)){
+  fs.mkdirSync(deployedCodePath);
+}
+//package to accept multipart form-data which allows clients to upload code and mapping files for execution
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, deployedCodePath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
+
+//accepts two files. one with the form name as "code" and another called "metadata"
+app.post('/deploy', upload.fields([{name: 'code'}, {name: 'metadata'}]), (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+
+  const codePath = req["files"]["code"][0]["path"];
+  const metadataPath = req["files"]["metadata"][0]["path"];
+
+  codeContainer.execute(codePath, metadataPath);
+  res.send();
 });
 
-//TODO code deployer stuff. move these
-
-// const deployedCodePath = `${__dirname}/deployed-code/`;
-// if (!fs.existsSync(deployedCodePath)){
-//   fs.mkdirSync(deployedCodePath);
-// }
-// //package to accept multipart form-data which allows clients to upload code and mapping files for execution
-// var multer  = require('multer');
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, deployedCodePath);
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   }
-// });
-// var upload = multer({ storage: storage });
-//
-// //accepts two files. one with the form name as "code" and another called "metadata"
-// app.post('/deploy', upload.fields([{name: 'code'}, {name: 'metadata'}]), (req, res) => {
-//   console.log(req.body)
-//   console.log(req.files);
-//
-//   const codePath = req["files"]["code"][0]["path"];
-//   const metadataPath = req["files"]["metadata"][0]["path"];
-//
-//   codeContainer.execute(codePath, metadataPath);
-//   res.send();
-// });
+//throw an error if it is an unknown endpoint
+app.use(function(req, res) {
+    res.status(404).send(`${req.originalUrl} is not a valid endpoint.`);
+});
