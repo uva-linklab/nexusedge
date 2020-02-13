e79509ca819f62bc6335a3a536cc11d10fed45f5
Nabeel Nasir <nabeeln7@gmail.com>
2020-02-12 23:29:11 -0500

    remove js keyword usage

--- utils/utils.js
+++ utils/utils.js
@@ -4,15 +4,15 @@ const pcap = require('pcap');
 const config = require('./config.json');
 
 function getIPAddress() {
-	const interface = config.network.interface;
-	if(!interface) {
+	const networkInterface = config.network.interface;
+	if(!networkInterface) {
 		console.log("interface not found in config file");
 	}
 
 	//regex to exclude ipv6 addresses and only capture ipv4 addresses. This doesn't ensure that the ipv4 octets are 0-255 but this would suffice. All we need is to exclude ipv6 addresses. 
 	const regex = /^\d+\.\d+\.\d+\.\d+$/;
 	return pcap.findalldevs()
-				.find(elem => elem.name === interface)
+				.find(elem => elem.name === networkInterface)
 				.addresses
 				.find(addrElem => addrElem && regex.test(addrElem.addr))
 				.addr;
