5686ce1f2c040e529c5b137264ef10525fea0c8c
HuangLiPang <leo89214749@hotmail.com>
2020-03-19 22:30:30 -0400

    [WIP] add app-manager
    TODO:
    1. oracle register initialization need to be reimplement.
    2. maybe use ipc to pass data to app, not topic.

---
+++ app-manager/code-container/container.js
@@ -0,0 +1,29 @@
+const fs = require('fs-extra');
+// const { spawn } = require('child_process');
+const path = require("path");
+
+exports.setApp = async function (appPath, metadataPath) {
+	const executableDirPath = `${__dirname}/executables`;
+	//create "executables" directory if not present
+	fs.ensureDirSync(executableDirPath);
+
+	//create a new directory in executables and copy the script and meta
+	const dirName = Date.now();
+	const dirPath = `${executableDirPath}/${dirName}`;
+	fs.ensureDirSync(dirPath);
+
+	const scriptTargetPath = `${dirPath}/${path.basename(appPath)}`;
+	const metadataTargetPath = `${dirPath}/${path.basename(metadataPath)}`;
+	const oracleSourcePath = `${__dirname}/oracle`;
+	const oracleTargetPath = `${dirPath}/oracle`;
+
+	fs.copySync(appPath, scriptTargetPath);
+	console.log(`${appPath} copied to ${scriptTargetPath}`);
+	fs.copySync(metadataPath, metadataTargetPath);
+	console.log(`${metadataPath} copied to ${metadataTargetPath}`);
+	fs.copySync(oracleSourcePath, oracleTargetPath);
+	console.log(`oracle library at ${oracleSourcePath} copied to ${oracleTargetPath}`);
+
+	// TODO: npm install here
+	return scriptTargetPath;
+}
