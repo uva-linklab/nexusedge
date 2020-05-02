28ea640d4be8177485c7eb7c236e6c22d4cac56f
HuangLiPang <leo89214749@hotmail.com>
2020-05-02 18:27:24 -0400

    bug fixed in app-manager: 1. add fs-extra module 2. add logs dir check 3. fix appPath variable typo

--- scripts/start-edge-platform.sh
+++ scripts/start-edge-platform.sh
@@ -1,5 +1,5 @@
 #on-the-edge scripts
-node $HOME/on-the-edge/api-server/server.js > $HOME/on-the-edge/logs/api-server.log 2>&1 &
+node $HOME/on-the-edge/platform/platform-manager.js > $HOME/on-the-edge/logs/platform-manager.log 2>&1 &
 DEBUG=gateway-scanner node $HOME/on-the-edge/gateway-scanner/gateway-scanner.js 2> $HOME/on-the-edge/logs/gw-scanner.log &
 node $HOME/on-the-edge/mqtt-data-collector/mqtt-data-collector.js &
 node $HOME/on-the-edge/ble-peripheral-scanner/ble-peripheral-scanner.js &
