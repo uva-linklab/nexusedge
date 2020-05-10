d88f7a4c203db7a37dd885837b41ff11667b7337
Nabeel Nasir <nabeeln7@gmail.com>
2020-05-10 16:04:30 -0400

    Multiple instances of noble cannot coexist if one of them connects to a peripheral. There needs to be major fix for this which makes ble-peripheral-scanner and lab11/gateway usable. Fix temporarily by not using these modules.
    Bug reported here:
    https://trello.com/c/iZ8Sihh3

--- scripts/start-edge-platform.sh
+++ scripts/start-edge-platform.sh
@@ -1,8 +1,10 @@
 #on-the-edge scripts
 node $HOME/on-the-edge/platform/platform-manager.js > $HOME/on-the-edge/logs/platform-manager.log 2>&1 &
 node $HOME/on-the-edge/mqtt-data-collector/mqtt-data-collector.js &
-node $HOME/on-the-edge/ble-peripheral-scanner/ble-peripheral-scanner.js &
+#Multiple noble processes cannot co-exist when there is some peripheral connection. Temporarily not using other noble
+#process.
+#node $HOME/on-the-edge/ble-peripheral-scanner/ble-peripheral-scanner.js &
 
 #lab11 gateway scripts for BLE and EnOcean sensors
-node $HOME/gateway/software/ble-gateway-mqtt/ble-gateway-mqtt.js &
+#node $HOME/gateway/software/ble-gateway-mqtt/ble-gateway-mqtt.js &
 node $HOME/gateway/software/enocean-generic-gateway/enocean-generic-gateway.js &
