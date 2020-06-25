8416df0d4cd8460bc8755594a1359268a9dea5db
Nabeel Nasir <nabeeln7@gmail.com>
2020-06-25 06:24:45 -0400

    Minor debug print change

--- platform/app-manager/code-container/oracle/examples/query-app/listen.js
+++ platform/app-manager/code-container/oracle/examples/query-app/listen.js
@@ -3,7 +3,8 @@ const oracle = new Oracle();
 
 oracle.on('query-all', function(tag, data) {
 	if(tag === 'queryData') {
-		console.log(`obtained query - ${data}`);
+		console.log("obtained query");
+		console.log(data);
 		const replyTag = data["_meta"]["reply-tag"];
 		oracle.disseminateAll(replyTag, {
 			"data": "queryResponseGoesHere"
