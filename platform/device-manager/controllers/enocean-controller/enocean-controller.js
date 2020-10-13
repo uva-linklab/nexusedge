22aa57aa5c3d8e4c6a313e24af55db3cb21a5e34
Nabeel Nasir <nabeeln7@gmail.com>
2020-10-13 03:26:53 -0400

    fix format of controller

--- platform/device-manager/controllers/enocean-controller/enocean-controller.js
+++ platform/device-manager/controllers/enocean-controller/enocean-controller.js
@@ -3,65 +3,87 @@
 const enocean = require('@nabeeln7/node-enocean')();
 const serialPort = require('serialport');
 
-serialPort.list()
-    .then(ports => {
-        ports.forEach(function (port) {
-            if (port.pnpId && port.pnpId.indexOf('EnOcean') !== -1) {
-                console.log('Using serial port ' + port.comName);
-                enocean.listen(port.comName);
+let instance = null;
+
+const initializeQueue = [];
+
+const subscriberCallbackList = [];
+
+class EnOceanController {
+    constructor() {
+        this._initialized = false;
+        this._initializing = false;
+    }
+
+    static getInstance() {
+        if(!instance) {
+            instance = new EnOceanController();
+        }
+        return instance;
+    }
+
+    /**
+     * Async function to initialize enocean controller
+     * @return {Promise<void>}
+     */
+    initialize() {
+        return new Promise((resolve, reject) => {
+            // if initialized, then return immediately
+            if(this._initialized) {
+                resolve();
+            } else if(this._initializing) { // if initialization underway, then wait in queue
+                initializeQueue.push(resolve);
+            } else {
+                this._initializing = true;
+
+                serialPort.list()
+                    .then(ports => {
+                        ports.forEach(port => {
+                            if (port.pnpId && port.pnpId.indexOf('EnOcean') !== -1) {
+                                console.log('Using serial port ' + port.path);
+                                enocean.listen(port.path);
+                                resolve();
+
+                                this._initializing = false;
+                                this._initialized = true;
+
+                                // resolve all pending promises
+                                initializeQueue.forEach(resolveFn => resolveFn());
+
+                                this._setupEnOceanEvents();
+                            }
+                        })
+                    })
+                    .catch(err => reject(err));
             }
         })
-    })
-    .catch(err => {
-        console.error('Error reading serial ports.');
-    });
-
-enocean.on("ready", function () {
-    console.log('Listening for EnOcean packets.');
-    enocean.startLearning();
-});
-
-enocean.on("learned", function (data) {
-    console.log('Learned about ' + data.eepType + '(' + data.id + ')');
-});
-
-enocean.on("known-data", function (data) {
-    const out = {
-        device: data.sensor.eepType,
-        _meta: {
-            received_time: new Date().toISOString(),
-            device_id: data.sensor.id,
-            receiver: 'enocean-generic-gateway',
-            gateway_id: _gateway_id
-        }
-    };
-
-    if (data.rssi) {
-        out.rssi = data.rssi;
     }
 
-    for (const shortName in data.data) {
-        const item = data.data[shortName];
-        // Skip any information about the learn bit.
-        if (shortName.indexOf('LRN') !== -1 || item.name.indexOf('Learn') !== -1) {
-            continue;
+    subscribe(callback) {
+        if(typeof callback === 'function') {
+            subscriberCallbackList.push(callback);
         }
-
-        // Otherwise add this to the packet.
-        let key = item.name;
-        if (item.unit) {
-            key += '_' + item.unit;
-        }
-
-        out[key] = item.value;
     }
 
-    console.log(out);
-});
+    _setupEnOceanEvents() {
+        enocean.on("ready", function () {
+            console.log('Listening for EnOcean packets.');
+            enocean.startLearning();
+        });
 
-enocean.on('learn-mode-stop', function (result) {
-    // If for any reason learning stops, start it again!
-    // Learning seems to stop for all sorts of reasons. Not good for a generic
-    // gateway!
-    enocean.startLearning();
-});
+        enocean.on("learned", function (data) {
+            console.log('Learned about ' + data.eepType + '(' + data.id + ')');
+        });
+
+        enocean.on("known-data", function (data) {
+            subscriberCallbackList.forEach(callbackFn => callbackFn(data));
+        });
+
+        enocean.on('learn-mode-stop', function (result) {
+            // If for any reason learning stops, start it again!
+            // Learning seems to stop for all sorts of reasons. Not good for a generic
+            // gateway!
+            enocean.startLearning();
+        });
+    }
+}
