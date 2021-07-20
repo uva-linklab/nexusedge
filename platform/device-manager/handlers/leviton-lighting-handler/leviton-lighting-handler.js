ba8d47bdd2f8872da4a7c2018673f2e724c555fd
Nabeel Nasir <nabeeln7@gmail.com>
2021-07-20 17:46:46 -0400

    cleaning up the leviton handler code

--- platform/device-manager/handlers/leviton-lighting-handler/leviton-lighting-handler.js
+++ platform/device-manager/handlers/leviton-lighting-handler/leviton-lighting-handler.js
@@ -1,17 +1,10 @@
-/*
-// TODO add which make of lighting sensors we are using
-Parses BLE packets from Lighting Sensor
- */
 const BleController = require('ble-controller');
 const bleController = BleController.getInstance();
 
-const SERVICE_CHANGED_CHAR_UUID = "2a05";
+const LEVITON_MAIN_SERVICE_UUID = "d027645004514000b0000000ee000000";
+
 const SECURE_CMD_TO_WRITE_CHAR_UUID = "d027645004514000b0000000ee030000"; // D0276450-0451-4000-B000-0000EE030000
 const SECURE_REPLY_NOTIFIED_CHAR_UUID = "d027645004514000b0000000ee040000"; // D0276450-0451-4000-B000-0000EE040000
-const FIRMWARE_VERSION_CHAR_UUID = "d027645004514000b0000000ee060000"; // D0276450-0451-4000-B000-0000EE060000
-const DEVICE_NAME_CHAR_UUID = "d027645004514000b0000000ee070000"; // D0276450-0451-4000-B000-0000EE070000
-
-let subscribedToSecureNotifications = false;
 
 // Stores any pending messages that need to be sent to a peripheral via BLE.
 // Type: deviceId -> [data]
@@ -27,16 +20,14 @@ class LevitonLightingHandler {
         this.isHandlingMessages = false;
         this.registeredDevices = [];
 
-        this.firmwareVersionChar = null;
-        this.deviceNameChar = null;
         this.secureCmdToWriteChar = null;
         this.secureReplyNotifiedChar = null;
-        this.serviceChangedChar = null;
     }
 
     start(platformCallback) {
         this.platformCallback = platformCallback;
         bleController.initialize().then(() => {
+            // we use the same way the app picks up leviton lights: by looking for $L$
             bleController.getPeripheralsWithPredicate(peripheral => {
                 const localName = peripheral.advertisement.localName;
                 return localName && localName.includes("$L$");
@@ -45,17 +36,6 @@ class LevitonLightingHandler {
     }
 
     dispatch(deviceId, data) {
-        /*
-        data must be of this format:
-        {
-            requestType: "stateControl" OR "brightnessControl"
-            payload: { "state": "on" OR "off" }, if requestType = stateControl
-            OR
-            payload: { "brightness": value }, if requestType = brightnessControl
-                value = [1, 25, 50, 75, or 100]
-        }
-         */
-
         // check if format is correct
         if(this._isValidData(data)) {
             // add to pendingMessages
@@ -120,24 +100,18 @@ class LevitonLightingHandler {
 
             await peripheral.connectAsync();
             await this._recordRelevantCharacteristics(peripheral);
-
-            console.log("going to subscribe to secureReplyNotifiedChar");
             await this.secureReplyNotifiedChar.subscribeAsync(); // this requires pairing
-            console.log("subscribed to secureReplyNotifiedChar");
 
             if(message["requestType"] === STATE_CONTROL_MSG_TYPE) {
                 const state = message["payload"]["state"] === "on";
                 await this._setLightState(peripheral, state);
             } else {
                 const brightnessLevel = message["payload"]["brightness"];
-                await this._setLightBrightness(peripheral, brightnessLevel);
+                await this._setBrightnessMaxThreshold(peripheral, brightnessLevel);
             }
 
             await this.secureReplyNotifiedChar.unsubscribeAsync();
-            console.log("unsubscribed from secureReplyNotifiedChar");
-
             await peripheral.disconnectAsync();
-            console.log("disconnected");
 
             if(messageList.length === 0) {
                 delete pendingMessages[peripheral.id];
@@ -148,29 +122,24 @@ class LevitonLightingHandler {
     }
 
     async _recordRelevantCharacteristics(peripheral) {
-        const response = await peripheral.discoverAllServicesAndCharacteristicsAsync();
-        response.characteristics.forEach(char => {
+        const services = await peripheral.discoverServicesAsync([LEVITON_MAIN_SERVICE_UUID]);
+        const levitonService = services.find(service => service.uuid === LEVITON_MAIN_SERVICE_UUID);
+        const charUuids = [SECURE_REPLY_NOTIFIED_CHAR_UUID, SECURE_CMD_TO_WRITE_CHAR_UUID];
+
+        const characteristics = await levitonService.discoverCharacteristicsAsync(charUuids);
+        characteristics.forEach(char => {
             switch (char.uuid) {
-                case SERVICE_CHANGED_CHAR_UUID:
-                    this.serviceChangedChar = char;
-                    break;
                 case SECURE_REPLY_NOTIFIED_CHAR_UUID:
                     this.secureReplyNotifiedChar = char;
                     break;
                 case SECURE_CMD_TO_WRITE_CHAR_UUID:
                     this.secureCmdToWriteChar = char;
                     break;
-                case FIRMWARE_VERSION_CHAR_UUID:
-                    this.firmwareVersionChar = char;
-                    break;
-                case DEVICE_NAME_CHAR_UUID:
-                    this.deviceNameChar = char;
-                    break;
             }
         });
     }
 
-    async _setLightState(peripheral, state) {
+    _setLightState(peripheral, state) {
         const commandsToSend = [];
         commandsToSend.push([0xFE, 0x03, 0x01, 0xE7, 0x4A]);
         commandsToSend.push([0xFE, 0x01, 0x02, 0x64, 0x10, 0x3C]); // needs to be sent every 5sec
@@ -178,17 +147,53 @@ class LevitonLightingHandler {
         commandsToSend.push([0xFE, 0x03, 0x01, 0xEA, 0xB7]);
         commandsToSend.push([0xFE, 0x03, 0x01, 0x11, 0xE3]);
         commandsToSend.push([0xFE, 0x01, 0x02, 0x64, 0x00, 0xA1]);
-        // commandsToSend.push([0xFE, 0x01, 0x02, 0x04, 0x02, 0x47]); // this seems to turn on things
-        // unnecessarily (commenting for the time being)
         if(state) {
-            commandsToSend.push([0xFE, 0x01, 0x02, 0x72, 0x02, 0x5B]); // turn the relay on. instead of 025B, 0A99 should also work.
+            // earlier, we used to turn the relay on using 025B or 0A99.
+            // this was what the manual control of the app would do.
+            // but I suspect the manual control is meant to control the light for 5mins.
+            // because, sending 25B or A99 caused the light to auto turn on 5 mins after the timeout period
+            commandsToSend.push([0xFE, 0x01, 0x02, 0x04, 0x02, 0x47]);
         } else {
             commandsToSend.push([0xFE, 0x01, 0x02, 0x72, 0x06, 0x3A]);  // turn the relay off. instead of 063A, 0EF8 should also work.
         }
-        await this._writeCommandsToLight(peripheral, commandsToSend);
+        return this._writeCommandsToLight(peripheral, commandsToSend);
     }
 
-    async _setLightBrightness(peripheral, level) {
+    //this is equivalent to setting a value in Dimming -> Max Threshold
+    _setBrightnessMaxThreshold(peripheral, level) {
+        const commandsToSend = [];
+        commandsToSend.push([0xFE, 0x03, 0x01, 0xE7, 0x4A]);
+        commandsToSend.push([0xFE, 0x01, 0x02, 0x64, 0x10, 0x3C]); // needs to be sent every 5sec
+        commandsToSend.push([0xFE, 0x03, 0x01, 0xE9, 0x55]);
+        commandsToSend.push([0xFE, 0x03, 0x01, 0xEA, 0xB7]);
+        commandsToSend.push([0xFE, 0x03, 0x01, 0x11, 0xE3]);
+
+        switch(level) {
+            case 1:
+                commandsToSend.push([0xFE, 0x01, 0x02, 0x11, 0x00, 0xE8]); // 1%
+                break;
+            case 25:
+                commandsToSend.push([0xFE, 0x01, 0x02, 0x11, 0x19, 0xE9]); // 25%
+                break;
+            case 50:
+                commandsToSend.push([0xFE, 0x01, 0x02, 0x11, 0x32, 0xEA]); // 50%
+                break;
+            case 75:
+                commandsToSend.push([0xFE, 0x01, 0x02, 0x11, 0x4B, 0x8E]); // 75%
+                break;
+            case 100:
+                commandsToSend.push([0xFE, 0x01, 0x02, 0x11, 0x64, 0xEC]); // 100%
+                break;
+            default:
+                console.error("Dimming state not supported");
+                return;
+        }
+        return this._writeCommandsToLight(peripheral, commandsToSend);
+    }
+
+    // this function should not be used. it temporarily sets the brightness level and resets it back after 5mins.
+    // use _setBrightnessMaxThreshold instead
+    _setLightBrightness(peripheral, level) {
         const commandsToSend = [];
         commandsToSend.push([0xFE, 0x03, 0x01, 0xE7, 0x4A]);
         commandsToSend.push([0xFE, 0x01, 0x02, 0x64, 0x10, 0x3C]); // needs to be sent every 5sec
@@ -218,7 +223,7 @@ class LevitonLightingHandler {
                 console.error("Dimming state not supported");
                 return;
         }
-        await this._writeCommandsToLight(peripheral, commandsToSend);
+        return this._writeCommandsToLight(peripheral, commandsToSend);
     }
 
     _writeCommandsToLight(peripheral, commands) {
