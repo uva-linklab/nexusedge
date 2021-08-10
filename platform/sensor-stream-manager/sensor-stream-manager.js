a7756882c52d02d9a67a7b90fb72264961129974
Nabeel Nasir <nabeeln7@gmail.com>
2021-08-09 23:04:13 -0400

    1. SSM seems to be picking up the device_id from within a _meta object (why?!), fixed it! 2. updated the "dirty" package to the latest version. This was done since I thought I saw a bug: db.rm(key) in dirty doesn't really remove the entry from the file on disk. But db.forEach() still works as expected with this key not showing up. So okay for now I guess?! Opened an issue for this in any case: https://github.com/felixge/node-dirty/issues/63

--- platform/sensor-stream-manager/sensor-stream-manager.js
+++ platform/sensor-stream-manager/sensor-stream-manager.js
@@ -149,7 +149,7 @@ function subscribeToGatewayData(client) {
 function routeSensorStreamsToApps(client) {
     client.on("message", (topic, message) => {
         const payload = JSON.parse(message.toString());
-        const sensorId = payload["_meta"]["device_id"];
+        const sensorId = payload["device_id"];
 
         if (sensorId in sensorStreamRouteTable) {
             for (const gatewayIp in sensorStreamRouteTable[sensorId]) {
