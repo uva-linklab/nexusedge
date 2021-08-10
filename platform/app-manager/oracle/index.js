c4c7a8fb15f25a06a5d53c0a859c752ff7623f09
Nabeel Nasir <nabeeln7@gmail.com>
2021-08-10 08:37:37 -0400

    add api for receiveType, receiveAny

--- platform/app-manager/oracle/index.js
+++ platform/app-manager/oracle/index.js
@@ -4,41 +4,93 @@ const mqttController = MqttController.getInstance();
 const httpUtils = require('./http-utils');
 
 let gatewayDetails = undefined;
-const callbackMap = {};
+
+const deviceIdCallbackMap = {};
+const deviceTypeCallbackMap = {};
+let anyDeviceCallback = null;
 
 class Oracle extends EventEmitter {
     constructor() {
         super();
-        const platformApiTopic = 'platform-data'; // this topic is used for disseminate and query apis
-        const applicationTopic = process.env.APP_DATA_TOPIC; // receive the application's topic as an environment variable
+        // this topic is used for disseminate and query apis
+        const platformApiTopic = 'platform-data';
+
+        // this topic is used for receiving the application's data streams
+        // the topic name is received as an environment variable
+        const applicationTopic = process.env.APP_DATA_TOPIC;
 
         if(!applicationTopic) {
             console.error("Application did not receive a topic from App Manager. Exiting.");
             process.exit(1);
         }
 
-        // if a new data is published in the topic, oracle will check if the application is listening for that deviceId
-        // if so, it sends the message to the callback function
+        // if new data is published in the topic for the app, oracle will provide this data based on how the app
+        // requested for the data (specific deviceId, deviceType, or any device)
         mqttController.subscribe("localhost", applicationTopic, message => {
-            const messageJson = JSON.parse(message);
+            const messageJson = JSON.parse(message.toString());
             const deviceId = messageJson['device_id'];
-            if(deviceId in callbackMap) {
-                callbackMap[deviceId](messageJson);
+            const deviceType = messageJson['device_type'];
+
+            if(deviceId in deviceIdCallbackMap) {
+                deviceIdCallbackMap[deviceId](messageJson);
+            }
+            if(deviceType in deviceTypeCallbackMap) {
+                deviceTypeCallbackMap[deviceType](messageJson);
+            }
+            if(anyDeviceCallback !== null) {
+                anyDeviceCallback(messageJson);
             }
         });
 
+        // if we receive any query or disseminate messages, emit them
         mqttController.subscribe("localhost", platformApiTopic, message => {
-            const messageJson = JSON.parse(message);
+            const messageJson = JSON.parse(message.toString());
             const api = messageJson._meta.api;
             const tag = messageJson._meta.tag;
             this.emit(api, tag, messageJson);
         });
     }
 
+    /**
+     * Receive a callback when there's new data available for a specific device
+     * @param deviceId id of the device of interest
+     * @param callback
+     */
     receive(deviceId, callback) {
-        callbackMap[deviceId] = callback;
-        console.log(`[oracle] added callback for ${deviceId}`);
+        if(typeof callback === 'function') {
+            deviceIdCallbackMap[deviceId] = callback;
+            console.log(`[oracle] added callback for ${deviceId}`);
+        } else {
+            throw Error('callback not a function');
+        }
     };
+
+    /**
+     * Receive a callback when there's new data available for any device of a specified type (for instance: "powerMeter")
+     * @param deviceType the type of the devices of interest
+     * @param callback
+     */
+    receiveType(deviceType, callback) {
+        if(typeof callback === 'function') {
+            deviceTypeCallbackMap[deviceType] = callback;
+            console.log(`[oracle] added callback for ${deviceType}`);
+        } else {
+            throw Error('callback not a function');
+        }
+    }
+
+    /**
+     * Receive a callback when there's new data available for any device
+     * @param callback
+     */
+    receiveAny(callback) {
+        if(typeof callback === 'function') {
+            anyDeviceCallback = callback;
+            console.log(`[oracle] added callback for data from any device`);
+        } else {
+            throw Error('callback not a function');
+        }
+    }
 
     send(deviceId, data) {
         const execUrl = `http://localhost:5000/gateway/talk-to-manager`;
