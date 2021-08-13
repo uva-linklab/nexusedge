a669ef7d7fe9bf99b747f8d4e2f66cf82bcac96d
Nabeel Nasir <nabeeln7@gmail.com>
2021-08-13 15:21:40 -0400

    perform datatype conversions for the SIF cloud data

--- platform/data-publish-manager/publishers/sif-cloud/sif-cloud-publisher.js
+++ platform/data-publish-manager/publishers/sif-cloud/sif-cloud-publisher.js
@@ -34,22 +34,107 @@ class SIFCloudPublisher {
         formattedData['counter'] = 0;
 
         const payloadFields = {};
-        for(const [fieldName, value] of Object.entries(sensorData['device_data'])) {
-            payloadFields[fieldName] = {
-                "displayName": fieldName,
+        const metadataFields = {};
+
+        // set the directly available metadata fields
+        metadataFields["time"] = new Date().toISOString();
+        metadataFields["handler_id"] = sensorData["_meta"]["handler_id"];
+        metadataFields["controller_id"] = sensorData["_meta"]["controller_id"];
+        metadataFields["gateway_address"] = sensorData["_meta"]["gateway_address"];
+        metadataFields["deviceType"] = sensorData["device_type"];
+
+        /*
+        OpenTSDB only supports integer or floating point metric values.
+        reference: http://opentsdb.net/docs/build/html/user_guide/writing/index.html
+        so we divide some of the device_data fields into payload fields and metadata fields
+        based on their datatype. we also flatten nested objects.
+        this conversion is explained in the jsdoc for _getOpenTsdbFriendlyFields.
+         */
+
+        const fields = this._getOpenTsdbFriendlyFields("", sensorData['device_data']);
+        for(const [key, value] of Object.entries(fields.payload)) {
+            payloadFields[key] = {
+                "displayName": key,
                 "unit": "",
                 "value": value
             };
         }
-        formattedData["metadata"] = {
-            "time": new Date().toISOString(),
-            "handler_id": sensorData["_meta"]["handler_id"],
-            "controller_id": sensorData["_meta"]["controller_id"],
-            "gateway_address": sensorData["_meta"]["gateway_address"],
-            "deviceType": sensorData["device_type"]
-        };
+        for(const [key, value] of Object.entries(fields.metadata)) {
+            metadataFields[key] = value;
+        }
         formattedData["payload_fields"] = payloadFields;
+        formattedData["metadata"] = metadataFields;
         return formattedData;
+    }
+
+    // reference: https://www.codegrepper.com/code-examples/javascript/javascript+check+if+string+is+number
+    _isNumeric(str) {
+        // Check if input is string
+        if (typeof str != "string")
+            return false;
+        return !isNaN(str) && !isNaN(parseFloat(str))
+    }
+
+    /**
+     * for a given key-value pair, returns the set of payload fields and metadata fields based on
+     the type of the value
+     - if value is a number (int/float), add to payload fields
+     - if value is a string, add as a metadata field (tag)
+     - if value is an object, recursively get fields for each key and append each key of the object to the external key
+        * eg: for "acceleration": {"x": 5, "y": 10, "unit": "mph"}
+            payload fields -> "acceleration_x": 5, "acceleration_y": 10
+            metadata fields -> "acceleration_unit": "mph"
+        * if there are nested objects, then do this recursively
+     - if value is a boolean, convert to int
+     * @param key
+     * @param value
+     * @return {{metadata: {}, payload: {}}}
+     * @private
+     */
+    _getOpenTsdbFriendlyFields(key, value) {
+        const output = {
+            "payload": {},
+            "metadata": {}
+        };
+
+        switch(typeof value) {
+            case 'number':
+                // add to payload fields
+                output["payload"][key] = value;
+                break;
+            case 'string':
+                // check if this can be type converted to a number
+                if(this._isNumeric(value)) {
+                    // then convert it!
+                    output["payload"][key] = parseFloat(value);
+                } else {
+                    // if not, add it as a metadata field
+                    output["metadata"][key] = value;
+                }
+                break;
+            case 'boolean':
+                // convert to int and add to payload field
+                output["payload"][key] = value ? 1 : 0;
+                break;
+            case 'object':
+                if(value !== null) {
+                    // iterate over each key-value pair and recursively obtain the payload and metadata fields
+                    for(const [innerKey, innerValue] of Object.entries(value)) {
+                        const innerOutput = getFields(innerKey, innerValue);
+                        // append the key to each payload/metadata field in innerOutput
+                        for(const [payloadField, payloadValue] of Object.entries(innerOutput.payload)) {
+                            const newKey = key === '' ? payloadField : `${key}_${payloadField}`;
+                            output["payload"][newKey] = payloadValue;
+                        }
+                        for(const [metadataField, metadataValue] of Object.entries(innerOutput.metadata)) {
+                            const newKey = key === '' ? metadataField : `${key}_${metadataField}`;
+                            output["metadata"][newKey] = metadataValue;
+                        }
+                    }
+                    break;
+                }
+        }
+        return output;
     }
 }
 
