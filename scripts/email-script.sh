c35d2f8ab27fe3eb830e74cb40e4a93c85cadabd
Nabeel Nasir <nabeeln7@gmail.com>
2021-09-01 13:30:04 -0400

    update email script

--- scripts/email-script.sh
+++ scripts/email-script.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#expects the board's serial number in the env var BOARD_SERIAL
-source /root/.bash_profile
+filename="/etc/gateway-id"
+if [ -s "$filename" ]
+then
+   id=`cat /etc/gateway-id`
+else
+   id=`cat /sys/class/net/wlan0/address | sed 's/://g'`
+fi
+
 echo -e "\
-From: "Artik Status" <artikstatus@gmail.com>\n\
+From: "NexusEdge" <artikstatus@gmail.com>\n\
 To: "Nabeel Nasir" <nn5rh@virginia.edu>\n\
-Subject: Board $BOARD_SERIAL IP\n\
-\n$(hostname -I|cut -d" " -f1)" |\
+Subject: Gateway On\n\
+\nid: $id\n \
+\nip: $(hostname -I|cut -d" " -f1)" |\
 curl -s -n --ssl-reqd --url "smtps://smtp.gmail.com:465" -T - \
-	-u artikstatus@gmail.com:artik_linklab \
-	--mail-from "artikstatus@gmail.com" \
-	--mail-rcpt "nn5rh@virginia.edu"
+        -u artikstatus@gmail.com:artik_linklab \
+        --mail-from "artikstatus@gmail.com" \
+        --mail-rcpt "nn5rh@virginia.edu"
