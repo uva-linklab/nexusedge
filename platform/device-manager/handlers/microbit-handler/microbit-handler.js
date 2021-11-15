7fc1973872cb2c988f22a2ae7ad8c332d0db2c58
Nabeel Nasir <nabeeln7@gmail.com>
2021-11-15 17:26:09 -0500

    handle microbit company id endianness properly

--- platform/device-manager/handlers/microbit-handler/microbit-handler.js
+++ platform/device-manager/handlers/microbit-handler/microbit-handler.js
@@ -35,16 +35,13 @@ class MicrobitHandler {
         // console.log(`[microbit-handler] discovered a peripheral with address = ${peripheral.id}`);
         // if it has a localName, add it as metadata
         const localName = peripheral.advertisement.localName;
-
         const manufacturerData = peripheral.advertisement.manufacturerData;
-        let bufferIndex = 0;
 
         // this does not include the length and AD Type bytes. so first two bytes would be company id
-        const companyId = manufacturerData.slice(0, 2).toString('hex');
-        bufferIndex+=2;
+        const companyId = manufacturerData.readUInt16LE(0).toString(16).padStart(4, '0');
 
         // convert rest of the payload buffer to string
-        const payloadBuffer = manufacturerData.slice(bufferIndex).toString('hex');
+        const payloadBuffer = manufacturerData.slice(2).toString('hex');
         const data = {
             "manufacturer_data": payloadBuffer, // will go as payload
             "local_name": localName, // this will end up as metadata,
