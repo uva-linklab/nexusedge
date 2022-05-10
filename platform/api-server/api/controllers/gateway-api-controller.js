b9c5ca81c66c20f435223bc6d35d1a2933f6de3e
Marshall Clyburn <mardev2013@gmail.com>
2022-05-10 15:53:55 +0000

    Revert more changes in the gateway API

--- platform/api-server/api/controllers/gateway-api-controller.js
+++ platform/api-server/api/controllers/gateway-api-controller.js
@@ -106,12 +106,14 @@ exports.getResources = async function(req, res) {
 exports.executeApp = async function(req, res) {
     const appPath = req["files"]["app"][0]["path"];
     const metadataPath = req["files"]["metadata"][0]["path"];
+    const runtime = req.body.runtime;
 
     // Forward the application path and metadata.
     // The data format is described in the platform-manager.js
-    messagingService.forwardMessage(serviceName, "app-manager", "execute-app", {
+    messagingService.forwardMessage(serviceName, "app-manager", "deploy-app", {
         "appPath": appPath,
         "metadataPath": metadataPath,
+        "runtime": runtime,
     });
     res.send();
 };
