6e8cd824c5e3b6e29fab590e74d20b5427521d70
Marshall Clyburn <mardev2013@gmail.com>
2022-07-28 10:02:57 -0400

    Make config appropriate for testing

--- platform/device-manager/gateway-scanner/talk-to-manager-service/talk-to-manager-service.js
+++ platform/device-manager/gateway-scanner/talk-to-manager-service/talk-to-manager-service.js
@@ -4,8 +4,9 @@ const bleno = require('@abandonware/bleno');
 
 // This service contains a single characteristic which takes a JSON message that needs to be passed between gateways
 const MessageCharacteristic = require('./message-characteristic').Characteristic;
-
-const serviceUUID = '18338db15c5841cca00971c5fd792920';
+// Testing group ID.
+// Change this to another value to uniquify the group.
+const serviceUUID = 'd7454f65f589478c88c566ec4a430b6d';
 
 function TalkToManagerService(messagingService, onWriteRequestFinished) {
     bleno.PrimaryService.call(this, {
